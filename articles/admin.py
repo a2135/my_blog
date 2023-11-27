3faa54129d980a6bd15ac900a5471cea5dfefe7a
alex b <ds_205@icloud.com>
2023-11-27 21:58:01 +0300

    comments added

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
 
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInline,]
     list_display = ("title", "date", "author",)
     
     
